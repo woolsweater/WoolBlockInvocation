e0fdefb57823f34608d1c47e9b3b6a7c5f67b4e6
Josh Caswell <josh@kraken>
2013-07-14 12:41:50 -0700

    Initial Commit

---
+++ BlockGlue/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BlockGlue
+//
+//  Created by Joshua Caswell on 7/14/13.
+//  Copyright (c) 2013 Josh Caswell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
